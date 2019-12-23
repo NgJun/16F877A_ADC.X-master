d173302e52d949185523ff8a13845d10e4874413
NgJun <57037817+NgJun@users.noreply.github.com>
2019-12-23 16:19:46 +0700

    ADC comment
    ADC comment

--- adc.c
+++ adc.c
@@ -8,28 +8,34 @@ void Delay_adc(int cnt)
 }
 void Init_ADC()
 { 
-    //  Fosc/64 =  20/64 Mhz
+    // Ch?n t?n s? chuy?n ??i ADC
+    //  Fosc/64
     ADCON0bits.ADCS0 = 0;
     ADCON0bits.ADCS1 = 1;
     ADCON1bits.ADCS2 = 1;
     
+    // Ch?n kênh giao ti?p : AN0
     ADCON0bits.CHS0=0;
     ADCON0bits.CHS1=0;
     ADCON0bits.CHS2=0;
     
-    ADCON1bits.ADFM = 1;
+    
+    ADCON1bits.ADFM = 1; // c?n l? ph?i
+    
+    // T?t c? chân AN là input Analog
     ADCON1bits.PCFG0 = 0;
     ADCON1bits.PCFG1 = 0;
     ADCON1bits.PCFG2 = 0;
     ADCON1bits.PCFG3 = 0;
     
-    ADCON0bits.GO_DONE = 1;
-    ADCON0bits.ADON = 1;
+    ADCON0bits.GO_DONE = 1; // Tr?ng thái 0 là busy, 1 là r?nh và s? làm bh
+    ADCON0bits.ADON = 1;        // b?t cái ADC lên
 }
+
 float read_ADC()
 {
     float data = 0;
-    data =  ((ADRESH << 8) + ADRESL);  
-    data = (data/1023.0 * 100); 
+    data =  ((ADRESH << 8) + ADRESL);   // 
+    data = (data/1023.0 * 100);                 // 2^10 -1
     return data;
 }
