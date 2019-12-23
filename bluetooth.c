ee0853a0c2890a167cc710be06962ea24ae80f2b
NgJun <57037817+NgJun@users.noreply.github.com>
2019-12-23 15:45:08 +0700

    update comment
    Update comment

--- bluetooth.c
+++ bluetooth.c
@@ -2,25 +2,35 @@
 
 void Initialize_Bluetooth()    
 {
-   //Set the pins of RX and TX//
-    TRISC6=1;
+     /*
+     * Set the pins of RX and TX//
+    *set TX is input hence in order to transmit data we have to load the Rx buffer of BT and then broadcast it using carriage return
+    *
+    * The Bluetooth module has a Transmit and Receive buffer inside it, the data sent to it will be stored in the Tx buffer. 
+     * This data will not be broadcasted (sent on air) unless a carriage return is sent to the module.
+     */
+    TRISC6=1;    
     TRISC7=1;
     
     //Set the baud rate using the look up table in datasheet(pg114)//
     // Baud rate cua hau het các bluetooth module là 9600
+    
     BRGH=1;      //BRGH enabled : high speed, baud rate ( B?t bu?c)
     SPBRG  =129;    // Tra bang BaudRate = 9600 và FREQ = 20MHz thì SPBRG = 129
     
     //Turn on Asyc. Serial Port//
     /*
      * SYNC: USART Mode Select bit
-     * 1 = Synchronous mode
-     * 0 = Asynchronous mode
+     * 1 = Synchronous mode, Sync in master mode: half duplex
+     * 0 = Asynchronous mode = full duplex
+     * In asSync mode, it just send data signal only, without clock signal
      */
+    // h?u h?t ai cx dùng cái này
     SYNC=0;
-    SPEN=1;
+    SPEN=1; // 1 = Serial port enabled (configures RC7/RX/DT and RC6/TX/CK pins as serial port pins)
+  /* AsSyc Serial communication */     
     
-    //Set 8-bit reception and transmission
+   //Set 8-bit reception and transmission
     /*
      * TX9: 9-bit Transmit Enable bit
      * 1 = Selects 9-bit transmission/reception
@@ -29,8 +39,9 @@ void Initialize_Bluetooth()
     RX9=0;
     TX9=0;
     
+    // Both sending and receiving
     TXEN=1;  // Enable transmit
-    CREN=1; //Enable Continuous Receive
+    CREN=1; //Enable receive
     
     //Enable global and ph. interrupts//
     GIE = 1;
@@ -43,7 +54,7 @@ void Initialize_Bluetooth()
     
 void BT_load_char(char byte)
 {
-    while(TXIF==0); // Doi den khi register transmitter trong   
+    while(TXIF==0);  // Doi den khi register transmitter trong   
     while(TRMT==0);// Doi den khi Trans Shift Reg trong
     TXREG = byte; // Load char need to send to reg of  trans
 }
@@ -60,15 +71,16 @@ void broadcast_BT()
 }
 char BT_get_char(void)
 {
-    if(OERR) // check for over run error 
+    if(OERR == 1 ) // check for over run error 
     {
         CREN = 0;
         CREN = 1; //Reset CREN
     }
     
-    if(RCIF==1) //if the user has sent a char return the char (ASCII value)
+    // 
+    if(RCIF==1) //if the user has sent a char return the char (ASCII value) // Doi den khi register transmitter trong   
     {
-    while(!RCIF);     // Wait till the data is received 
+    while( RCIF == 0);     // Wait till the data is received 
     return RCREG;
     }
     else //if user has sent no message return 0
--- main.c
+++ main.c
@@ -15,12 +15,20 @@
 
 void Pin_Init()
 {
-    TRISA = 0xFF;
-    TRISB = 0;
-    PORTB = 0xFF;
+    TRISA = 0xFF;   // t?t c? c?ng A là input
+    
+    TRISB = 0x00;         // T?t c? c?ng B là d?ng output  
+    
+    PORTB = 0xFF;       // T?t c? chân ? c?ng B = 1
+    
+    // TRISB1 = 0  chân B1 là thu?c d?ng output
+    // PORTB1= 1 thì là B1 nó ?ang mu?n output ngay bh = mu?n cho dòng ?i?n 25mA ra chân B1
+    //PORTB1 = 0 B1 không mu?n output ngay bh
+    
     __delay_ms(200);
     PORTB = 0;
-    TRISD = 0;       
+    
+    TRISD = 0;   // T?t c? chân D là output    
 }
 
 void main(void) {
@@ -40,20 +48,25 @@ void main(void) {
     int i=0;
     while(1)
     {
-        blt_mess = BT_get_char();
-	if( blt_mess == '0' ) PORTB = 0;
-        if( blt_mess == '1' ) PORTB = 0xFF;        
-        if( blt_mess == '2' ) LED_mode= 0;
+        blt_mess = BT_get_char();  // L?y trong không khí tín hi?u Bluetooth g?i v?
+        // t? dt.   -> RX
+        // Chuy?n data trong RX sang bi?t "blt_mess"
+        if( blt_mess == '0' ) PORTB = 0;    // T?t ?èn
+        if( blt_mess == '1' ) PORTB = 0xFF;        // Sáng ?èn
+        
+        if( blt_mess == '2' ) LED_mode= 0;      // B?t t?t ch? ?? "?èn sáng khi có nh?p tim"
         if( blt_mess == '3' ) LED_mode = 1;
-	if( blt_mess == '4' ) send_mode= 0;
+        
+	if( blt_mess == '4' ) send_mode= 0; //
         if( blt_mess == '5' ) send_mode = 1;
+        
        if(ADCON0bits.GO_DONE == 0 ) 
         {
            time++;
             ADCON0bits.GO_DONE = 1;
-            data = read_ADC();
+            data = read_ADC(); // 0-> 100
             if(LED_mode == 1)
-                if(data>62  && time > ðŸ˜Ž 
+                if(data>62  && time > 8   ) 
             {
                 time = 0;
                 PORTB = 0xFF;
@@ -63,11 +76,18 @@ void main(void) {
             
             if(send_mode == 1)
             {
-            sprintf(mess, "%.2f",data);
-            BT_load_string(mess);
-            broadcast_BT();
+                
+                
+            sprintf(mess, "%.2f",data);    
+            // data là d?ng float -> string( m?ng các ph?n t? char ) l?u giá tr? c?a data
+            // vào m?t bi?n dàng string tên là mess
+            // float   data  12.34 -> mess  "12.64"
             
+            BT_load_string(mess);   // N?p d? li?u string c?n g?i vào thanh ghi ?êm TX
+            broadcast_BT();             // G?i t?t c? m?i th? ?ang ch?a trong thanh TX
             __delay_ms(50);
+      
+            
             }
           
         }
